a0c60d145c2ab5ded5a001929e42a50f8c76c9af
Samim.Azimi <samim.elite8@gmail.com>
2022-03-26 16:50:30 +0430

    option added

--- routes/records.routes.js
+++ routes/records.routes.js
@@ -10,7 +10,7 @@ router.post('/record', (req, res) => {
         res.send(err._message)
     
     })
-}) 
+})  
 
 router.get('/allrecords', (req, res) => {
     var findRecord = RecordModel.find().limit(100).then(result => {
@@ -25,10 +25,72 @@ router.post('/recordName', (req, res) => {
     console.log(req.body)
     var findRecord = RecordModel.find({ 'siteName': req.body.siteName }).limit(100).then(result => {
         res.send(result)
+       
     }).catch(err => {
         console.log(err)
         res.send(err._message)
     })
 })
 
+router.post('/options', (req, res) => {
+
+    console.log(req.body)
+    if(req.body.options ==2){
+    var findRecord = RecordModel.find({ 'siteContactNumber': req.body.query }).limit(100).then(result => {
+        res.send(result)
+       
+    }).catch(err => {
+        console.log(err)
+        res.send(err._message)
+    })
+    }
+      if(req.body.options ==3){
+    var findRecord = RecordModel.find({ 'hardware.MakeModel': req.body.query }).limit(100).then(result => {
+        res.send(result)
+       
+    }).catch(err => {
+        console.log(err)
+        res.send(err._message)
+    })
+    }
+       if(req.body.options ==4){
+    var findRecord = RecordModel.find({ 'hardware.ServiceTagSerialNo': req.body.query }).limit(100).then(result => {
+        res.send(result)
+       
+    }).catch(err => {
+        console.log(err)
+        res.send(err._message)
+    })
+    }
+    if(req.body.options ==5){
+    var findRecord = RecordModel.find({ 'apps.appsName': req.body.query }).limit(100).then(result => {
+        res.send(result)
+       
+    }).catch(err => {
+        console.log(err)
+        res.send(err._message)
+    })
+    }
+    if(req.body.options ==6){
+    var findRecord = RecordModel.find({ 'apps.appsVersion': req.body.query }).limit(100).then(result => {
+        res.send(result)
+       
+    }).catch(err => {
+        console.log(err)
+        res.send(err._message)
+    })
+    }
+      if(req.body.options ==7){
+    var findRecord = RecordModel.find({ 'apps.appsVersion': req.body.query }).limit(100).then(result => {
+        res.send(result)
+       
+    }).catch(err => {
+        console.log(err)
+        res.send(err._message)
+    })
+    }
+
+}) 
+
 module.exports = router
+
